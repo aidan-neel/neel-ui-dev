9a65d0a77f34fe97121b6267cb7e9b0747917d14
Aidan Neel <aaidan5899@gmail.com>
2024-04-18 01:31:10 -0500

    Components folder

---
+++ components/textarea/textarea.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    import { cn } from "$library/utils";
+	import { globalStateStore } from "$library/state";
+	import { getContext } from "svelte";
+
+	type $$Props = {
+		class?: string;
+		placeholder?: string;
+		value?: string;
+	} & typeof $$restProps;
+
+	let className: $$Props["class"] = undefined;
+	export let placeholder: $$Props["placeholder"] = "";
+	export let value: $$Props["value"] = "";
+	export { className as class };
+</script>
+
+<textarea placeholder={placeholder} value={value} {...$$restProps} class={cn("w-full p-3 rounded-lg border-default bg-alternate-muted focus:outline-none focus:border-white/20 placeholder:text-foreground-muted text-foreground", className)} />
---
+++ components/tooltip/index.ts
@@ -0,0 +1,13 @@
+import Root from "./tooltip.svelte";
+import Content from "./content.svelte";
+import Trigger from "./trigger.svelte";
+
+export {
+	Root,
+	Content,
+	Trigger,
+	
+	Root as Tooltip,
+	Content as TooltipContent,
+	Trigger as TooltipTrigger,
+};
